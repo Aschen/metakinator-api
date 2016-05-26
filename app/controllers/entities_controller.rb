42417ba81e6fa8369dee28f66c99f8d77b55c6c0
Aschen <amaret93@gmail.com>
2016-05-26 16:08:47 +0200

    Fix param verification for add_entity
    [77]

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -124,7 +124,7 @@ class EntitiesController < ApplicationController
   end
 
   def add_entity
-    if params[:entity_name].empty? || params[:questions].empty?
+    if params[:entity_name].blank? || params[:questions].blank?
       render json: { "errors" => "Missing entity_name or questions param" }, status: 400 and return
     end
 
